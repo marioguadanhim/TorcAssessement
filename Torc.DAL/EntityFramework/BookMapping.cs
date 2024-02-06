55e76786b15b2153b624cba78ba7c95d9c77b998
marioguadanhim <mario_aurelio_321@hotmail.com>
2024-02-06 10:18:31 -0300

    Adding db

--- Torc.DAL/EntityFramework/BookMapping.cs
+++ Torc.DAL/EntityFramework/BookMapping.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection.Emit;
 
 namespace Torc.DAL.EntityFramework
 {
@@ -13,6 +14,8 @@ namespace Torc.DAL.EntityFramework
         public void Configure(EntityTypeBuilder<Book> builder)
         {
             builder.HasKey(x => x.book_id);
+
+            builder.Property(u => u.book_id).ValueGeneratedOnAdd();
 
             builder.ToTable("books");
         }
---
+++ Torc.DAL/Migrations/20240206131127_init.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Torc.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    book_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    total_copies = table.Column<int>(type: "int", nullable: false),
+                    copies_in_use = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isbn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    category = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.book_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+        }
+    }
+}
